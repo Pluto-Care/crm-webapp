06f3f144e46166683bdc993805759d3e09fff9db
Gauravjot Garaya <gauravjot@outlook.com>
2024-06-23 02:36:00 -0700

    change: filtering the query result

--- src/services/api/patients/my/list.ts
+++ src/services/api/patients/my/list.ts
@@ -16,6 +16,10 @@ export function getMyPatientListAPI() {
 			withCredentials: true,
 		})
 		.then((response) => {
-			return response.data.data as PatientType[];
+			const list = response.data.data as PatientType[];
+			// TODO: [ONLY needed as distinct does not work with SQLite]
+			// remove duplicate ids
+			const uniqueList = list.filter((v, i, a) => a.findIndex((t) => t.id === v.id) === i);
+			return uniqueList;
 		});
 }
